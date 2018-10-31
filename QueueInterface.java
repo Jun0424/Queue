06b7901c802be74bde9f0b1e22918b618964a3b9
Jun0424 <41717915+Jun0424@users.noreply.github.com>
2018-10-31 16:37:26 +0800

    Add files via upload

---
+++ QueueInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queue;
+
+/**
+ *
+ * @author Jun
+ */
+public interface QueueInterface {
+    public void add(Object object);
+    public Object first();
+    public Object remove();
+    public int size();
+    public boolean isEmpty();
+}
